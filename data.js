68365eb70b320cae0cfc0c8cea3006cda84e5095
William Thompson <68480511+Frosty1212@users.noreply.github.com>
2020-10-30 07:58:40 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,339 @@
+
+// Request Methods for API calling
+
+const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+};
+
+const requestOptions2 = {
+    'method': 'GET',
+    'redirect': 'follow',
+	"headers": {
+		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+		"x-rapidapi-key": "ef3650d504mshb0cc17ee10eee5ap18f7bajsn6e531365fca0"
+	}
+};
+
+// 2 Different API's calling for team data, 1 JSON Object customized for seeds.
+
+const nbaTeamRankedArr = [
+    {
+        "seed": 14,
+        "abbreviation": "ATL",
+        "city": "Atlanta",
+        "conference": "East",
+        "division": "Southeast",
+        "full_name": "Atlanta Hawks",
+        "name": "Hawks"
+    },
+    {
+        "seed": 3,
+        "abbreviation": "BOS",
+        "city": "Boston",
+        "conference": "East",
+        "division": "Atlantic",
+        "full_name": "Boston Celtics",
+        "name": "Celtics"
+    },
+    {
+        "seed": 7,
+        "abbreviation": "BKN",
+        "city": "Brooklyn",
+        "conference": "East",
+        "division": "Atlantic",
+        "full_name": "Brooklyn Nets",
+        "name": "Nets"
+    },
+    {
+        "seed": 10,
+        "abbreviation": "CHA",
+        "city": "Charlotte",
+        "conference": "East",
+        "division": "Southeast",
+        "full_name": "Charlotte Hornets",
+        "name": "Hornets"
+    },
+    {
+        "seed": 11,
+        "abbreviation": "CHI",
+        "city": "Chicago",
+        "conference": "East",
+        "division": "Central",
+        "full_name": "Chicago Bulls",
+        "name": "Bulls"
+    },
+    {
+        "seed": 15,
+        "abbreviation": "CLE",
+        "city": "Cleveland",
+        "conference": "East",
+        "division": "Central",
+        "full_name": "Cleveland Cavaliers",
+        "name": "Cavaliers"
+    },
+    {
+        "seed": 7,
+        "abbreviation": "DAL",
+        "city": "Dallas",
+        "conference": "West",
+        "division": "Southwest",
+        "full_name": "Dallas Mavericks",
+        "name": "Mavericks"
+    },
+    {
+        "seed": 3,
+        "abbreviation": "DEN",
+        "city": "Denver",
+        "conference": "West",
+        "division": "Northwest",
+        "full_name": "Denver Nuggets",
+        "name": "Nuggets"
+    },
+    {
+        "seed": 13,
+        "abbreviation": "DET",
+        "city": "Detroit",
+        "conference": "East",
+        "division": "Central",
+        "full_name": "Detroit Pistons",
+        "name": "Pistons"
+    },
+    {
+        "seed": 15,
+        "abbreviation": "GSW",
+        "city": "Golden State",
+        "conference": "West",
+        "division": "Pacific",
+        "full_name": "Golden State Warriors",
+        "name": "Warriors"
+    },
+    {
+        "seed": 4,
+        "abbreviation": "HOU",
+        "city": "Houston",
+        "conference": "West",
+        "division": "Southwest",
+        "full_name": "Houston Rockets",
+        "name": "Rockets"
+    },
+    {
+        "seed": 4,
+        "abbreviation": "IND",
+        "city": "Indiana",
+        "conference": "East",
+        "division": "Central",
+        "full_name": "Indiana Pacers",
+        "name": "Pacers"
+    },
+    {
+        "seed": 2,
+        "abbreviation": "LAC",
+        "city": "LA",
+        "conference": "West",
+        "division": "Pacific",
+        "full_name": "LA Clippers",
+        "name": "Clippers"
+    },
+    {
+        "seed": 1,
+        "abbreviation": "LAL",
+        "city": "Los Angeles",
+        "conference": "West",
+        "division": "Pacific",
+        "full_name": "Los Angeles Lakers",
+        "name": "Lakers"
+    },
+    {
+        "seed": 9,
+        "abbreviation": "MEM",
+        "city": "Memphis",
+        "conference": "West",
+        "division": "Southwest",
+        "full_name": "Memphis Grizzlies",
+        "name": "Grizzlies"
+    },
+    {
+        "seed": 5,
+        "abbreviation": "MIA",
+        "city": "Miami",
+        "conference": "East",
+        "division": "Southeast",
+        "full_name": "Miami Heat",
+        "name": "Heat"
+    },
+    {
+        "seed": 1,
+        "abbreviation": "MIL",
+        "city": "Milwaukee",
+        "conference": "East",
+        "division": "Central",
+        "full_name": "Milwaukee Bucks",
+        "name": "Bucks"
+    },
+    {
+        "seed": 14,
+        "abbreviation": "MIN",
+        "city": "Minnesota",
+        "conference": "West",
+        "division": "Northwest",
+        "full_name": "Minnesota Timberwolves",
+        "name": "Timberwolves"
+    },
+    {
+        "seed": 13,
+        "abbreviation": "NOP",
+        "city": "New Orleans",
+        "conference": "West",
+        "division": "Southwest",
+        "full_name": "New Orleans Pelicans",
+        "name": "Pelicans"
+    },
+    {
+        "seed": 12,
+        "abbreviation": "NYK",
+        "city": "New York",
+        "conference": "East",
+        "division": "Atlantic",
+        "full_name": "New York Knicks",
+        "name": "Knicks"
+    },
+    {
+        "seed": 5,
+        "abbreviation": "OKC",
+        "city": "Oklahoma City",
+        "conference": "West",
+        "division": "Northwest",
+        "full_name": "Oklahoma City Thunder",
+        "name": "Thunder"
+    },
+    {
+        "seed": 8,
+        "abbreviation": "ORL",
+        "city": "Orlando",
+        "conference": "East",
+        "division": "Southeast",
+        "full_name": "Orlando Magic",
+        "name": "Magic"
+    },
+    {
+        "seed": 6,
+        "abbreviation": "PHI",
+        "city": "Philadelphia",
+        "conference": "East",
+        "division": "Atlantic",
+        "full_name": "Philadelphia 76ers",
+        "name": "76ers"
+    },
+    {
+        "seed": 10,
+        "abbreviation": "PHX",
+        "city": "Phoenix",
+        "conference": "West",
+        "division": "Pacific",
+        "full_name": "Phoenix Suns",
+        "name": "Suns"
+    },
+    {
+        "seed": 8,
+        "abbreviation": "POR",
+        "city": "Portland",
+        "conference": "West",
+        "division": "Northwest",
+        "full_name": "Portland Trail Blazers",
+        "name": "Trail Blazers"
+    },
+    {
+        "seed": 12,
+        "abbreviation": "SAC",
+        "city": "Sacramento",
+        "conference": "West",
+        "division": "Pacific",
+        "full_name": "Sacramento Kings",
+        "name": "Kings"
+    },
+    {
+        "seed": 11,
+        "abbreviation": "SAS",
+        "city": "San Antonio",
+        "conference": "West",
+        "division": "Southwest",
+        "full_name": "San Antonio Spurs",
+        "name": "Spurs"
+    },
+    {
+        "seed": 2,
+        "abbreviation": "TOR",
+        "city": "Toronto",
+        "conference": "East",
+        "division": "Atlantic",
+        "full_name": "Toronto Raptors",
+        "name": "Raptors"
+    },
+    {
+        "seed": 6,
+        "abbreviation": "UTA",
+        "city": "Utah",
+        "conference": "West",
+        "division": "Northwest",
+        "full_name": "Utah Jazz",
+        "name": "Jazz"
+    },
+    {
+        "seed": 9,
+        "abbreviation": "WAS",
+        "city": "Washington",
+        "conference": "East",
+        "division": "Southeast",
+        "full_name": "Washington Wizards",
+        "name": "Wizards"
+    }
+]
+const nbaTeamDataArr = []
+
+var nbaTeamRankedSorted = nbaTeamRankedArr.sort((a, b) => (a.seed > b.seed) ? 1 : -1)
+
+   fetch("https://www.balldontlie.io/api/v1/teams", requestOptions)
+    .then(response => {
+       return response.json()
+    }) 
+    .then(result => getTeamData(result.data))
+    .catch(error => console.log(error));
+
+function getTeamData(data){
+    data.map(function(currentTeam){
+        nbaTeamDataArr.push(currentTeam)
+    })
+}
+
+const nbaPlayerDataArr = []
+
+    fetch("https://balldontlie.io/api/v1/players", requestOptions)
+    .then(response => {
+       return response.json()
+    }) 
+    .then(result => getPlayerData(result.data))
+    .catch(error => console.log(error));
+
+
+// Player Finder API
+
+function getSpecificPlayer(first, last){
+    fetch("https://api-nba-v1.p.rapidapi.com/players/league/standard", requestOptions2)
+    .then(response => {
+        return response.json();
+    })
+    .then(result => {
+        printPlayerBio(result.api.players, first, last)
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+function findPlayerStats(first, last){
+    fetch(`https://nba-players.herokuapp.com/players-stats/${last}/${first}`, requestOptions)
+        .then(response => response.json())
+        .then(result => printPlayerStats(result))
+        .catch(error => console.log('error', error));
+}
---
+++ script.js
@@ -0,0 +1,251 @@
+//Runs functons to render the information on the screen on DOM load
+
+function onDOMLoad(){
+  renderTeamStandingsWest(nbaTeamRankedSorted);
+  renderTeamStandingsEast(nbaTeamRankedSorted);
+  picList(nbaTeamDataArr); 
+  renderTeams(nbaTeamDataArr);
+  getSeed(); 
+}
+
+//Renders the logo and rosters for each team
+
+function renderTeams(nbaTeamDataArr) {
+  var logoWall = document.getElementById('logoWall')
+  var teamLogos = nbaTeamDataArr.map((currentTeam) => {
+    return `
+      <div id="${currentTeam.name}" class="logoContainer">  
+        <img id="teamLogo" src="https://stats.nba.com/media/img/teams/logos/${currentTeam.abbreviation}_logo.svg">
+        ${currentTeam.full_name}<br> [${currentTeam.abbreviation}]
+        <a href="https://en.wikipedia.org/wiki/${currentTeam.full_name}" target="_blank">Learn More</a>
+        <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#collapseExample${currentTeam.id}" aria-expanded="false" aria-controls="collapseExample${currentTeam.teamId}">Roster</button>
+        <div class="team-Card collapse" id="collapseExample${currentTeam.id}">
+          <div class="card card-body">
+            <div class="team-roster" id="team-roster">
+              <div class="teamRoster-title">
+                <h4>${currentTeam.name} Roster:</h4>
+              </div>
+              <div class="players">
+                ${findPlayerRoster(currentTeam.name)}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>`
+  })
+logoWall.innerHTML = teamLogos.join(` `)
+}
+
+// Renders all the team standings
+
+function renderTeamStandingsWest (nbaTeamDataArr) {
+  var rankedTeam = document.getElementById(`rankedTeamWestern`)
+  var newTeamArr = nbaTeamDataArr.map((currentTeam) => {
+    if (findTeamConference(currentTeam.conference) == `West`) {
+     return `
+     <div class="teamRankedContainer" ${findTeamRank(currentTeam.name)}>
+       <div class="card-top">
+         <img id="teamLogo2" src="https://stats.nba.com/media/img/teams/logos/${currentTeam.abbreviation}_logo.svg">
+         <div>
+          <h4 class="seed${currentTeam.conference}" id="seed-${currentTeam.name}"></h4>
+         </div>
+         <div class="team-body">
+           <div class="teamName">
+             <h4><a id="team-link" href="#${currentTeam.name}">${currentTeam.full_name}</a></h4>
+             <h6>${currentTeam.division}</h6>
+           </div>
+         </div>
+       </div>
+     </div>
+     `
+    }else {
+      return
+    }
+  })
+rankedTeam.innerHTML = newTeamArr.join(` `)
+}
+
+function renderTeamStandingsEast (nbaTeamDataArr) {
+  var rankedTeam = document.getElementById(`rankedTeamEastern`)
+  var newTeamArr = nbaTeamDataArr.map((currentTeam) => {
+    if (findTeamConference(currentTeam.conference) == `East`) {
+     return `
+      <div class="teamRankedContainer" id="${findTeamRank(currentTeam.name)}">
+        <div class="card-top">
+          <img id="teamLogo2" src="https://stats.nba.com/media/img/teams/logos/${currentTeam.abbreviation}_logo.svg">
+          <div>
+            <h4 class="seed${currentTeam.conference}" id="seed-${currentTeam.name}"></h4>
+          </div>
+          <div class="team-body">
+            <div class="teamName">
+              <h4><a id="team-link" href="#${currentTeam.name}">${currentTeam.full_name}</a></h4>
+              <h6>${currentTeam.division}</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+     `
+    }else {
+      return
+    }
+  })
+    rankedTeam.innerHTML = newTeamArr.join(` `)
+}
+
+// Finds the rosters for each team in the renderTeams function above
+
+function findPlayerRoster(playerTeam){
+  var newPlayerRoster = nbaPlayerDataArr.map(currentPlayer => {
+    if(playerTeam == currentPlayer.team.name){
+      return `
+      <div class="playerBar">
+      ${currentPlayer.first_name + ' ' + currentPlayer.last_name} POS:${currentPlayer.position}
+      </div>
+      `
+    }
+  })
+  return newPlayerRoster
+}
+
+//returns team rank for standing
+
+function findTeamRank(currentTeamName){
+  nbaTeamRankedArr.map(function(rankedTeam){
+    if(rankedTeam.name.toString() == currentTeamName){
+      newSeed = `${rankedTeam.seed.toString()}`
+      return newSeed
+    }else {
+    }
+  })
+}
+
+function getSeed(){
+  nbaTeamRankedArr.map(function(rankedTeam){
+    var newTeamSeed = document.getElementById(`seed-${rankedTeam.name}`)
+    newTeamSeed.innerHTML = `${rankedTeam.seed.toString()}`
+  })
+}
+
+//sorts the teams by conference for standings
+
+function findTeamConference(team){
+  return `${team}`
+}
+
+//the toggle for profile popup
+
+function profilePopup() {
+  var x = document.getElementById("profile");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  document.getElementById("userPic2").innerHTML = null;
+}
+
+//Sets users pic to the team logo chosen
+
+function picList(nbaTeams) {
+  nbaTeams.map(function(team) { 
+    let teamName = document.getElementById('teamSelector');
+    let teamElement = document.createElement('option');
+    teamElement.setAttribute('value', team.abbreviation);
+    teamElement.innerHTML = team.name;        
+    teamName.appendChild(teamElement);
+  });
+};  
+
+function getTeamPic2(nbaTeams) {
+  return  `<img id="userImg2" src="https://stats.nba.com/media/img/teams/logos/${nbaTeams}_logo.svg">`
+}
+
+function getTeamPic(nbaTeams) {
+  return  `<img id="userImg" src="https://stats.nba.com/media/img/teams/logos/${nbaTeams}_logo.svg">`
+}
+
+var changeUserPic = document.getElementById('teamSelector');
+changeUserPic.addEventListener("change", () => {
+  let teamName = document.getElementById('teamSelector');
+  newTeamName = teamName.value;
+  document.getElementById("userPic").innerHTML = getTeamPic(newTeamName);    
+  document.getElementById("userPic2").innerHTML = getTeamPic2(newTeamName);
+  document.getElementById("userFav").innerHTML = console.log(setFavTeam(newTeamName, nbaTeamDataArr))
+});
+
+// Displays chosen teams name in profile popup
+
+function setFavTeam(selectTeam) {
+    return `Favorite Team: ${selectTeam}`
+}
+
+// Sets Username 
+
+function userNameChanger(){
+  var name = document.getElementById('changeUsername').value
+  document.getElementById('username1').innerHTML = `${name}`
+  document.getElementById('user2').innerHTML = `${name}`
+}
+
+//Decides what to do when a user doesn't save the changes
+
+function notSave() {
+  userpic1 = document.getElementById("userPic")
+  userpic2 = document.getElementById("userPic2")
+  if(userpic1 != null && userpic2 != null) {
+    userpic1.innerHTML = null
+  }else{
+    return console.log('Error at notSave')
+  }
+  profilePopup();
+}
+
+// Below prints all the data for player finder, sorted by bio and stats
+
+function printPlayerBio(playerData, playerGivenFirst, playerGivenLast){
+  playerData.map(function(newArr){
+    if(newArr.firstName.toString() == playerGivenFirst.charAt(0).toUpperCase() + playerGivenFirst.slice(1) && playerGivenLast.charAt(0).toUpperCase() + playerGivenLast.slice(1) == newArr.lastName.toString()){
+      document.getElementById("playerImg").innerHTML = `<img id="playerDisplayImg" src="https://nba-players.herokuapp.com/players/${newArr.lastName + "/" + newArr.firstName}">`
+      document.getElementById("playerPosition").innerHTML = `Positon: ${newArr.leagues.standard.pos} | Jersey: #${newArr.leagues.standard.jersey}`
+      document.getElementById("playerDisplayName").innerHTML = `${newArr.firstName + ' ' + newArr.lastName}`
+      document.getElementById('playerSecondary').innerHTML = `College: ${newArr.collegeName} | Born: ${newArr.dateOfBirth}`
+    }else{
+      return console.log('Error at printPlayerBio')
+    }
+  })
+}
+
+function printPlayerStats(data){
+  document.getElementById('ppg').innerHTML = data.points_per_game
+  document.getElementById('apg').innerHTML = data.assists_per_game
+  document.getElementById('rpg').innerHTML = data.rebounds_per_game
+  document.getElementById('spg').innerHTML = data.steals_per_game
+  document.getElementById('bpg').innerHTML = data.blocks_per_game
+  document.getElementById('topg').innerHTML = data.turnovers_per_game
+  document.getElementById('mpg').innerHTML = data.minutes_per_game
+  document.getElementById('fapg').innerHTML = data.field_goals_attempted_per_game
+  document.getElementById('fmpg').innerHTML = data.field_goals_made_per_game
+  document.getElementById('ft').innerHTML = `${data.free_throw_percentage}%`
+  document.getElementById('fg').innerHTML = `${data.field_goal_percentage}%`
+  document.getElementById('3p').innerHTML = `${data.three_point_percentage}%`
+}
+
+// Key popup function
+
+function showKey() {
+  var x = document.getElementById('graphKey');
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+//gets the input data to search for the player
+
+function userPlayerInput(){
+  var first = document.getElementById('firstName').value
+  var last = document.getElementById('lastName').value
+  getSpecificPlayer(first, last)
+  findPlayerStats(first, last)
+}
